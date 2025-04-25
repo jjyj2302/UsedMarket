86e55252c117c966c7128e860a999bc551d26406
jjyj2302 <jjyj2302@gmail.com>
2025-04-25 23:07:20 +0900

    After fixing all errors

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from .routes.main import main_bp
 from .routes.product import product_bp
 from .routes.admin import admin_bp
 from .routes.chat import chat_socket_events
+from flask_wtf.csrf import generate_csrf
 import os
 
 socketio = SocketIO()
@@ -48,7 +49,15 @@ def create_app():
 
     @app.after_request
     def add_security_headers(response):
-        response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self'"
+        response.headers['Content-Security-Policy'] = (
+            "default-src 'self' https://cdn.socket.io; "
+            "script-src 'self' 'unsafe-inline' https://cdn.socket.io; "
+            "style-src 'self' 'unsafe-inline'; "
+        )
         return response
 
+    # 템플릿에서 {{ csrf_token() }} 사용 가능하도록 설정
+    @app.context_processor
+    def inject_csrf_token():
+        return dict(csrf_token=generate_csrf)
     return app
--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,12 +1,11 @@
 # app/routes/chat.py
 from flask import session
-from flask_socketio import send
+from flask_socketio import emit
 import uuid
 from app.utils.decorators import login_required
 
 def chat_socket_events(socketio):
     @socketio.on('send_message')
-    @login_required
     def handle_send_message_event(data):
         data['message_id'] = str(uuid.uuid4())
-        send(data, broadcast=True)
+        emit('message',data, broadcast=True)
